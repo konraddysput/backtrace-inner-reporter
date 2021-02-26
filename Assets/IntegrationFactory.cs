8c15c1eaa60fa03bcca4746918b71e2ed82356d6
konraddysput <konrad.dysput@gmail.com>
2021-02-26 14:59:22 +0100

    Fixed asmdef + return definition

--- Assets/IntegrationFactory.cs
+++ Assets/IntegrationFactory.cs
@@ -3,14 +3,16 @@ using Backtrace.Unity.Model;
 using UnityEngine;
 
 public class IntegrationFactory {
-    public static void CreateErrorReporter() {
+    public static BacktraceClient CreateErrorReporter() {
         var configuration = ScriptableObject.CreateInstance<BacktraceConfiguration>();
         configuration.ServerUrl = "https://yolo.sp.backtrace.io:6098/post?format=json&token=533c6e267998b8562e4b878c891bf7fc509beec7839f991bdaa1d43220d0f497";
         configuration.Enabled = true;
         configuration.PerformanceStatistics = true;
         configuration.DatabasePath = "${Application.persistentDataPath}/backtrace";
         configuration.CreateDatabase = true;
+#if UNITY_ANDROID || UNITY_IOS
         configuration.CaptureNativeCrashes = true;
+#endif
 
         return BacktraceClient.Initialize(configuration);
     }
